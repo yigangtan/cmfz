e1a1f7270ce28a0e05d939ef33ca2ba178c38884
yigangtan <2546772102@qq.com>
2018-11-27 16:11:13 +0800

    init project v1.0

---
+++ src/main/java/com/baizhi/cmfz/CmfzApplication.java
@@ -0,0 +1,32 @@
+package com.baizhi.cmfz;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+
+@MapperScan("com.baizhi.cmfz.dao")
+@SpringBootApplication
+public class CmfzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzApplication.class, args);
+    }
+    @Bean
+    public HttpMessageConverters fastjsonHttpMessageConverter(){
+        //定义一个转换消息的对像
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //添加fastjson的配置信息 比如 ：是否要格式化返回的json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //在转换器中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
+}
